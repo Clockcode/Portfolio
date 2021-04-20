6105d603280220540eee34e57245df76cd8cfa2a
Clockcode <cagdasmuldur@gmail.com>
2021-04-20 01:47:45 -0400

    modifying app.listen configuration for hosting

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const path = require("path");
 // set up the template engine
 app.set("views", "./views");
 app.set("view engine", "pug");
+// sets port 8080 to default or unless otherwise specified in the environment
+app.set("port", process.env.PORT || 8080);
 
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -23,7 +25,4 @@ app.get("/notes", (req, res) => {
   res.render("views/notes.pug");
 });
 
-const PORT = 8000;
-app.listen(PORT, () => {
-  console.log(`App is listening on Port ${PORT}!`);
-});
+app.listen(app.get("port"));
