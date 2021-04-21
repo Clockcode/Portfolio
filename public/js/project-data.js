9a411a0525ac90e5e8dca9e0a633c3966072ac39
Clockcode <cagdasmuldur@gmail.com>
2021-04-21 10:50:08 -0400

    Modified desc in projects array

--- public/js/project-data.js
+++ public/js/project-data.js
@@ -14,7 +14,8 @@ const projects = [
     img: "lifestraw",
     page_link: "lifestraw",
     github_link: "#",
-    figma_link: "#",
+    figma_link:
+      "https://www.figma.com/proto/ExTcf28THDNrPqXo7AUJz3/Product-Landing-Page-Day-3?page-id=0%3A1&node-id=2%3A21&viewport=-205%2C256%2C0.3363185524940491&scaling=scale-down",
     website_link: "#",
     desc: "lorem ipsum lorem ipsum lorem ipsum lorem ipsum",
   },
@@ -23,7 +24,8 @@ const projects = [
     img: "journal-app",
     page_link: "journal-app",
     github_link: "#",
-    figma_link: "#",
+    figma_link:
+      "https://www.figma.com/proto/2khf7C5fWWXfo84ub9Wm76/Journal-Home-Screen?page-id=0%3A1&node-id=3%3A2&viewport=399%2C228%2C0.8290854692459106&scaling=scale-down",
     website_link: "#",
     desc: "lorem ipsum lorem ipsum lorem ipsum lorem ipsum",
   },
@@ -32,7 +34,8 @@ const projects = [
     img: "notes-logo",
     page_link: "notes-app",
     github_link: "#",
-    figma_link: "#",
+    figma_link:
+      "https://www.figma.com/file/dKVuLrjE2kRU3ZyNGuVOB8/App-Logo-Design?node-id=0%3A1",
     website_link: "#",
     desc: "lorem ipsum lorem ipsum lorem ipsum lorem ipsum",
   },
