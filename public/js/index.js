921e63add01ea3e5b82cc59049845ae093623457
Clockcode <cagdasmuldur@gmail.com>
2021-05-27 18:19:42 -0400

    Adding animation to links, modifying welcome text and animating 'Hi!' part.

--- public/js/index.js
+++ public/js/index.js
@@ -6,25 +6,56 @@
 //   duration: "5000",
 //   easing: "easeInOutQuad",
 // });
-$(document).ready(function () {
-  function removeLoading() {
-    anime.timeline({}).add({
-      targets: ".loader-container",
-      opacity: [1, 0],
-      easing: "easeInExpo",
-      zIndex: {
-        value: -1,
-      },
-      duration: 1000,
-    });
-    async function demo() {
-      await new Promise((r) => setTimeout(r, 5000));
-      let loaderContainer = document.querySelector(".loader-container");
-      loaderContainer.parentElement.removeChild(loaderContainer);
-    }
+$(document).ready(() => {
+  // Removes Loading
+  const removeLoading = async () => {
+    await new Promise((r) => setTimeout(r, 2000));
+    let loaderContainer = document.querySelector(".loader-container");
+    loaderContainer.parentElement.removeChild(loaderContainer);
+  };
 
-    demo();
-  }
+  anime({
+    targets: ".loader-container",
+    opacity: [1, 0],
+    easing: "easeInExpo",
+    zIndex: {
+      value: -1,
+    },
+    duration: 1000,
+  });
 
   removeLoading();
+
+  //   timeline created
+  let t1 = anime.timeline({
+    easing: "easeInExpo",
+  });
+
+  // hi! text gets bigger from the void
+  t1.add({
+    targets: ".animated-letter",
+    scale: [0, 1],
+    elasticity: 200,
+    delay: anime.stagger(400),
+    offset: "-=300",
+    duration: 1500,
+  });
+
+  let targetWidth = 768;
+
+  if ($(window).width() >= targetWidth) {
+    //Add your javascript for screens wider than or equal to 768 here
+
+    // Links are coming from left of the screen.
+    t1.add(
+      {
+        targets: ".intro__link",
+        translateX: [-150, 0],
+        duration: 1500,
+      },
+      "-=1500"
+    );
+  } else {
+    //Add your javascript for screens smaller than 768 here
+  }
 });
